66f101b4f0dac18e7be0224075e0ab00346f274a
Andrea Tupini <tupini07@gmail.com>
2017-01-13 13:53:02 +0100

    get feeds and process feeds

--- src/clb/core.clj
+++ src/clb/core.clj
@@ -1,7 +1,41 @@
 (ns clb.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as st]
+            [clb.feed-parse :as fp]))
+
+(defn get-feedlist []
+  "returns an array of feed URLs. This are taken from a 
+   file called 'feeds.txt' in the resources folder."
+  (-> (clojure.java.io/resource "feeds.txt")
+      .getPath
+      (st/replace #"%20" " ") ; adaptation for paths with spaces
+      slurp
+      (st/replace #"\r" "")
+      (st/split #"\n")))
+
+(defn process-feeds [feeds]
+  (let [all-wc (->> feeds ; Construct map consisting of all titles and their wc 
+                    (map fp/word-count)
+                    (apply concat)
+                    merge)
+        ttl-wc (->> (map :wc all-wc) ; get all wc maps 
+                    (apply merge-with +))] ; merge all wc maps with +
+    [all-wc ttl-wc]))
+
+
+(def feeds [
+            "https://gigaom.com/feed/"
+            "http://feeds.feedburner.com/37signals/beMH"
+            "http://feeds.feedburner.com/blogspot/bRuz"
+            ])
+
+(def pff (process-feeds feeds))
+(def a (first pff))
+(def b (second pff))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Cows"
   [& args]
-  (println "Hello, World!"))
+
+  )
