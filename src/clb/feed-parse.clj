b1f9a30775e725a362f12ab2f8223e8b7e783397
Andrea Tupini <tupini07@gmail.com>
2017-01-16 12:48:39 +0100

    create edn dataset

--- src/clb/feed-parse.clj
+++ src/clb/feed-parse.clj
@@ -17,9 +17,10 @@
        (map st/lower-case)))          ; Convert all to lower-case
 
 (defn word-count [url]
-  "given a URL of a feed it returns [{:title entry-title 
-                                      :words-counts {:word1 cnt1 ...}]"
-  (->> (parse-feed url)
+  "given a URL of a feed it returns a the wc off all words in all entries
+   the map has the form {word word-count}"
+  (def feed (parse-feed url))
+  (->> feed
        :entries
        (map (fn [ent] ;; do the following proces once for each entry
            ; if a summary of the entry exists then we use the entry to do our word count
@@ -36,6 +37,10 @@
                              (assoc mp wrd 1) ; associate value of word with 1
                              (assoc mp wrd (inc (mp wrd)))))] ; increment value of word
                 {:title ttle
-                 :wc    (reduce rf {} txt)})))))
+                 :wc    (reduce rf {} txt)})))
+       (map :wc) ; extract an array of maps of the keywords of each entry
+       (apply merge-with +)
+       (#(identity {:title (:title feed)
+                     :wc %})))) ; return a wc of all words in all entries
 
 (word-count "https://gigaom.com/feed/")
