585d3c1be89f075e4fdb4330fd887b4d3dd55988
Andrea Tupini <tupini07@gmail.com>
2017-01-16 13:15:24 +0100

    read dataset

--- src/clb/feed-process.clj
+++ src/clb/feed-process.clj
@@ -4,6 +4,8 @@
             [clojure.pprint :as pp]
             [clojure.edn :as edn]))
 
+; Name of the dataset
+(def fna "dtset.edn")
 
 (defn rsrc-path [fname]
   "Returns path to resource folder"
@@ -30,13 +32,13 @@
                     (apply merge-with +))] ; merge all wc maps with +
     [all-wc ttl-wc]))
 
+
 (defn get-wordlist [total-wc feedlist]
   "given the lenght of our wordlist and a map of all the worcounts
    we return an array of words whose percentage of ocurrence is between
    a range (not too common and not to rare) "
   (def upper-limit 0.5)
-  (def lower-limit 0.1)
-  
+  (def lower-limit 0.1)  
   (def feedlen (count feedlist))
   (->> total-wc
        (filter (fn [[w fr]] 
@@ -44,10 +46,10 @@
                    (< 0.1 frac 0.5))))
        (map first)))
 
+
 (defn create-dataset [all-wc wordlist]
   "Gets the result of applying fp/word-count to each feed and the wordlist
    to use. Filters out only words in the wordlist and saves all to an edn file."
-  (def fna "dtset.edn")
   (->> all-wc 
        (map (fn [blg]
               (def blog (:title blg))
@@ -66,7 +68,13 @@
        (#(identity {:wordlist wordlist
                     :blogs-wc %}))
        pr-str ; convert to string
-       (spit fna))) ; write to file
+       (spit (rsrc-path fna))))   ; write to file
+
+(defn read-dataset []
+  "returns a map with the following form {:wordlist [wordlist that is being used]
+                                          :bogs-wc [{:blog blog :wc wc}]}"
+  (read-string (slurp (rsrc-path fna))))
+
 
 (def feeds [ ; test feeds so we don't have to load aaaalll the feeds when testing
             "https://gigaom.com/feed/"
@@ -74,6 +82,7 @@
             "http://feeds.feedburner.com/blogspot/bRuz"
             ])
 
+;; (read-dataset)
 (def pff (process-feeds feeds))
 (def a (first pff))
 (def b (second pff))
