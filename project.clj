4b3f8775bcaa3986024d3d322b7c9af6021efc9a
Andrea Tupini <tupini07@gmail.com>
2017-01-18 12:28:31 +0100

    calc distances and find min distance

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :url "https://github.com/tupini07/classify-blogs"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojars.freemarmoset/feedparser-clj "0.6.1"]]
+                 [org.clojars.freemarmoset/feedparser-clj "0.6.1"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
   :main ^:skip-aot clb.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clb/clusters.clj
+++ src/clb/clusters.clj
@@ -1,9 +1,6 @@
 (ns clb.clusters
   (:require [clojure.string :as st]
-            [clb.rss-parse :as rsp]
-            [clojure.pprint :as pp]
-            [clojure.edn :as edn]
-            [clb.feed-process :as fp]))
+            [clojure.math.combinatorics :as comb]))
 
 ;; We have to imagine that the space in which the blogs are placed has (count wordlist)
 ;; dimensions. Initially each blog will be its own cluster. The clustering algorithm will
@@ -12,8 +9,6 @@
 ;; a UUID
 
 (defn uuid [] (str (java.util.UUID/randomUUID)))
-
-(defn make-cluster [])
 
 (defn pearson [pos1 pos2]
   "Used to calculate the closeness of 2 clusters. Pos is a map of form {dimension magnitude}"
@@ -39,5 +34,45 @@
       0
       (- 1 (/ numer denom)))))
 
-(pearson {"sado" 2 "weq" 23 "rework" 213 "rek" 324 "rweo" 231}
-         {"sado" 2 "weq" 23 "rework" 213 "rek" 324 "rweo" 231})
+
+(defn make-cluster 
+  "Imagine clusters as a tree where each node has 2 sub-leafs
+   Example: cluster of only 2 blogs
+    -> root ; cluster that contains all other nodes
+       -> cluster1 -> blog-1
+       -> cluster2 -> blog-2 "
+  ([nme pos] (merge {:blog nme} ; only leafs have blog names
+                    (make-cluster pos nil nil))) ; a leaf
+  ([pos left right]
+   {:id (uuid)
+    :pos pos
+    :left left
+    :right right}))
+
+(defn blogs->clusters 
+  "recieves an array of maps {:blog title :wc position} and returns an
+   array of clusters"
+  [blogs]
+  (->> blogs
+       (map #(make-cluster (:blog %) (:wc %)))))
+
+(defn calc-dists
+  "calculate the distances between all elements in space (all clusters)
+     space is an array of all clusters"
+  [space]
+  (defn single-distances 
+    "calculate distance between 2 clusters"
+    [cl1 cl2]
+    {:ids  #{(:id cl1) (:id cl2)} 
+     :dist (pearson (:pos cl1) (:pos cl2))})
+
+  ;; this could be more efficient but for ocmprehentions sake..
+  (let [poss-c (comb/combinations space)] ;; array of pairs of all possible combinations
+    (->> poss-c
+         (map (fn [[cl1 cl2]]
+                (single-distances cl1 cl2))))))
+
+(defn find-min-dist 
+  "Gets the already processed clusters and finds the pair with the least distance"
+  [prc-clsts]
+  (apply min-key :dist prc-clsts))
